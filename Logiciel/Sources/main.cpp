ad65f9ba94dc8ce720337744eb2f13c102f53eb0
Thomas COELHO <coelho_thomas@yahoo.fr>
2022-11-29 15:59:57 +0100

    Modification du main

---
+++ Logiciel/Sources/main.cpp
@@ -0,0 +1,13 @@
+//! \file main.cpp
+//! \brief main du logiciel
+//! \author Thomas.C
+//! \version 0.1
+//! \date 29 novembre 2022
+
+#include <string>
+#include <list>
+
+int main()
+{
+    return 0;
+} 
